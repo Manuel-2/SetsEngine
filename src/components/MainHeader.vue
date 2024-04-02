2382c86481b75fc307756ab28ea07f712471709f
Manuel Peñaloza <25lopezm25@gmail.com>
2024-04-02 06:50:28 -0600

    nueva fuente y se añadio el icono de la app

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,5 +1,6 @@
 <script>
 import HeaderButton from "./HeaderButton.vue";
+// import SetsEnineIconSvg from "../assets/Icons/SetsEngineIcon.svg";
 
 export default {
   name: "MainHeader",
@@ -12,6 +13,11 @@ export default {
 <template>
   <div class="header">
     <div class="header__part">
+      <img
+        class="header__app-logo"
+        src="../assets/Icons/SetsEngineIcon.svg"
+        alt="setsIcon"
+      />
       <h1 class="header__title">Sets Engine</h1>
     </div>
     <div class="header__part">
@@ -26,16 +32,20 @@ export default {
 .header {
   width: 100%;
   height: var(--header-height);
-  padding: 0px 15px;
+  padding: 0px 50px;
   display: flex;
   justify-content: space-between;
   background-color: var(--header-color);
-  box-shadow: 0px 5px 15px -7px rgba(0,0,0,0.1);;
+  box-shadow: 0px 5px 15px -7px rgba(0, 0, 0, 0.1);
 }
 
 .header__part {
   height: 100%;
   display: flex;
   align-items: center;
+}
+
+.header__app-logo{
+  height: 80%;
 }
 </style>
